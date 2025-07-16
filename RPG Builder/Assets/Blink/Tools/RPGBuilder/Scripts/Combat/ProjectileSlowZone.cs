e45b2c749355a700a2fdb3504679a7a52bb53c78
Thomas (Blink) <business@blinkstudios.dev>
2025-07-16 15:52:24 +0200

    Init RPGB

---
+++ RPG Builder/Assets/Blink/Tools/RPGBuilder/Scripts/Combat/ProjectileSlowZone.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using BLINK.RPGBuilder.LogicMono;
+using UnityEngine;
+
+public class ProjectileSlowZone : MonoBehaviour
+{
+
+    public float velocityModifier = 0.1f;
+
+    private class ProjectileList
+    {
+        public GameObject GO;
+        public Vector3 cachedVelocity;
+    }
+
+    private List<ProjectileList> projectileList = new List<ProjectileList>();
+    private void OnTriggerEnter(Collider other)
+    {
+        ProjectileHitDetection projRef = other.gameObject.GetComponent<ProjectileHitDetection>();
+        if (projRef == null) return;
+        if (projRef.RB == null) return;
+        ProjectileList newProjectileList = new ProjectileList();
+        newProjectileList.GO = other.gameObject;
+        newProjectileList.cachedVelocity = projRef.RB.linearVelocity;
+        projectileList.Add(newProjectileList);
+        projRef.RB.linearVelocity *= velocityModifier;
+    }
+    
+    private void OnTriggerExit(Collider other)
+    {
+        ProjectileHitDetection projRef = other.gameObject.GetComponent<ProjectileHitDetection>();
+        if (projRef == null) return;
+        if (projRef.RB == null) return;
+        foreach (var proj in projectileList)
+        {
+            if(proj.GO != other.gameObject) continue;
+            projRef.RB.linearVelocity = proj.cachedVelocity;
+            projectileList.Remove(proj);
+            break;
+        }
+    }
+}
